085d164220c821960c13d52b30d410868eddf398
Van Taymany <vtaymany@gmail.com>
2021-02-18 15:47:05 -0800

    places config inside of connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,12 @@
 var mysql = require('mysql')
 
 var connection = mysql.createConnection({
-  host: 'localhost',
+  host: 'us-cdbr-east-03.cleardb.com',
   port: 3306,
-  user: 'root',
-  password: 'welcome123',
+  user: 'b7fdb4b718e65f',
+  password: '532518ab',
   database: 'heroku_2e1a4b9d7fea309',
+  dialect: 'mysql',
 })
 
 // Make connection.
