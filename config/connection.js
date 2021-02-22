a4032d69960a9509d1bfc9569fcaab5aacda8b04
Van Taymany <vtaymany@gmail.com>
2021-02-22 11:41:49 -0800

    disconnect handling try 2

--- config/connection.js
+++ config/connection.js
@@ -28,43 +28,20 @@ module.exports = connection
 // Set up MySQL connection.
 var mysql = require('mysql')
 
-var db_config = {
+var mysql = require('mysql')
+var connection = mysql.createPool({
   host: 'us-cdbr-east-03.cleardb.com',
   port: 3306,
   user: 'b7fdb4b718e65f',
   password: '532518ab',
   database: 'heroku_2e1a4b9d7fea309',
   dialect: 'mysql',
-}
+})
 
-var connection
-
-function handleDisconnect() {
-  connection = mysql.createConnection(db_config) // Recreate the connection, since
-  // the old one cannot be reused.
-
-  connection.connect(function (err) {
-    // The server is either down
-    if (err) {
-      // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err)
-      setTimeout(handleDisconnect, 2000) // We introduce a delay before attempting to reconnect,
-    } // to avoid a hot loop, and to allow our node script to
-  }) // process asynchronous requests in the meantime.
-  // If you're also serving http, display a 503 error.
-  connection.on('error', function (err) {
-    console.log('db error', err)
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      // Connection to the MySQL server is usually
-      handleDisconnect() // lost due to either server restart, or a
-    } else {
-      // connnection idle timeout (the wait_timeout
-      throw err // server variable configures this)
-    }
-  })
-}
-
-handleDisconnect()
+connection.getConnection(function (err, connect) {
+  // connected! (unless `err` is set)
+  connect.end()
+})
 
 // Export connection for our ORM to use.
 module.exports = connection
